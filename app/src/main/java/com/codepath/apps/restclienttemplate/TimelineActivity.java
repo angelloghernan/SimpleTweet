677fae56eaccfe6c05f98bfed35f6a93b4e90db4
Angello Hernandez <a.gianni1234@gmail.com>
2021-06-28 16:21:54 -0700

    Add compose skeleton and place log out button on action bar

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,16 +1,21 @@
 package com.codepath.apps.restclienttemplate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 
@@ -46,6 +51,22 @@ public class TimelineActivity extends AppCompatActivity {
         populateHomeTimeline();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull @NotNull MenuItem item) {
+        if (item.getItemId() == R.id.compose) {
+            Toast.makeText(this, "Composed!", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId() == R.id.logout) {
+            onLogoutClicked();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void populateHomeTimeline() {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
@@ -68,7 +89,7 @@ public class TimelineActivity extends AppCompatActivity {
         });
     }
 
-    public void onLogoutClicked(View view) {
+    public void onLogoutClicked() {
         client.clearAccessToken();
         finish();
     }
